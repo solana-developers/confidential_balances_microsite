9effc3ae3401f0922fe9aed8bd5111ddf3cb80c6
Sergo <rogaldh@radsh.red>
2025-05-20 22:05:22 +0100

    feat: update UI-kit (#5)

--- frontend/Makefiles/storybook.mk
+++ frontend/Makefiles/storybook.mk
@@ -2,3 +2,7 @@
 sb:
 	@echo "Launching @solana/ms-tools-ui storybook..."
 	@pnpm --filter @solana/ms-tools-ui storybook
+
+# Script to allow generating boilerplate for UI-kit. Will be removed later on replacing UI-kit with external one
+generate:
+	@pnpm --filter @solana/ms-tools-ui run generate
