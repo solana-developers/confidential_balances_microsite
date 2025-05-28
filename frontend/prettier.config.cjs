7a3d7cb0cd96cabeb1f69879d6a1cf89b337ce33
Sergey Zwezdin <sergey@zwezdin.com>
2025-05-28 07:35:30 +0300

    Prettier configuration fix (#27)
    * fix: prettier configuration
    
    * fix: prettier configuration

---
+++ frontend/prettier.config.cjs
@@ -0,0 +1,3 @@
+const config = require('@hoodieshq/ms-tools-config/prettier.config.js')
+
+module.exports = config
--- frontend/prettier.config.mjs
+++
@@ -1,3 +0,0 @@
-import config from '@hoodieshq/ms-tools-config/prettier.config.js'
-
-export default config
